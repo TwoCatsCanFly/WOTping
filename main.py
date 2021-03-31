28c87cd420c759121de7b48feab4e63eac6714b1
Aleksandr Puchkov <aleksandr.puchkov@simbirsoft.com>
2021-03-31 10:03:17 +0300

    Ver 0.1 Basic structure

---
+++ main.py
@@ -0,0 +1,35 @@
+from os import popen
+
+ver = 0.1
+server_list = [
+    {'ip':'8.8.8.8','name':'GOOGLE.com'},
+    {'ip':'8.8.4.4','name':'Google.com'},
+]
+
+class WOTServer:
+    def __init__(self, ip, name):
+        self.ip = ip
+        self.name = name
+
+    def ping_me(self):
+        ping = popen(f"ping {self.ip}").read()
+        if "Received = 4" in ping:
+            print(f"{self.name} - {self.ip} is UP")
+        else:
+            print(f"{self.name} - {self.ip} is DOWN")
+
+
+def main():
+    print('WOTping start')
+    for server in server_list:
+        ip  =server['ip']
+        name  =server['name']
+        print(f'ip {ip} name {name}')
+        name = WOTServer(ip,name)
+        name.ping_me()
+
+    return True
+
+
+if __name__ == '__main__':
+    main()
